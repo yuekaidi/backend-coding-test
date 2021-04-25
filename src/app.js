31a439440bdbfcc7b48975b9ea978f0cf25e1a16
kaidi <kaidi@pand.ai>
2021-04-25 12:37:46 +0800

    Add testcases for all apis

--- src/app.js
+++ src/app.js
@@ -21,90 +21,62 @@ module.exports = (db) => {
 		const driverName = req.body.driver_name;
 		const driverVehicle = req.body.driver_vehicle;
 
-		if (
-			startLatitude < -90 ||
-      startLatitude > 90 ||
-      startLongitude < -180 ||
-      startLongitude > 180
-		) {
+		if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
 			return res.send({
 				error_code: "VALIDATION_ERROR",
-				message:
-          "Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively",
+				message: "Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively"
 			});
 		}
 
-		if (
-			endLatitude < -90 ||
-      endLatitude > 90 ||
-      endLongitude < -180 ||
-      endLongitude > 180
-		) {
+		if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
 			return res.send({
 				error_code: "VALIDATION_ERROR",
-				message:
-          "End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively",
+				message: "End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively"
 			});
 		}
 
 		if (typeof riderName !== "string" || riderName.length < 1) {
 			return res.send({
 				error_code: "VALIDATION_ERROR",
-				message: "Rider name must be a non empty string",
+				message: "Rider name must be a non empty string"
 			});
 		}
 
 		if (typeof driverName !== "string" || driverName.length < 1) {
 			return res.send({
 				error_code: "VALIDATION_ERROR",
-				message: "Rider name must be a non empty string",
+				message: "Driver name must be a non empty string"
 			});
 		}
 
 		if (typeof driverVehicle !== "string" || driverVehicle.length < 1) {
 			return res.send({
 				error_code: "VALIDATION_ERROR",
-				message: "Rider name must be a non empty string",
+				message: "Driver vehicle must be a non empty string"
 			});
 		}
 
-		var values = [
-			req.body.start_lat,
-			req.body.start_long,
-			req.body.end_lat,
-			req.body.end_long,
-			req.body.rider_name,
-			req.body.driver_name,
-			req.body.driver_vehicle,
-		];
+		var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
+        
+		const result = db.run("INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)", values, function (err) {
+			if (err) {
+				return res.send({
+					error_code: "SERVER_ERROR",
+					message: "Unknown error"
+				});
+			}
 
-		const result = db.run(
-			"INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)",
-			values,
-			function (err) {
+			db.all("SELECT * FROM Rides WHERE rideID = ?", this.lastID, function (err, rows) {
 				if (err) {
 					return res.send({
 						error_code: "SERVER_ERROR",
-						message: "Unknown error",
+						message: "Unknown error"
 					});
 				}
 
-				db.all(
-					"SELECT * FROM Rides WHERE rideID = ?",
-					this.lastID,
-					function (err, rows) {
-						if (err) {
-							return res.send({
-								error_code: "SERVER_ERROR",
-								message: "Unknown error",
-							});
-						}
-
-						res.send(rows);
-					}
-				);
-			}
-		);
+				res.send(rows);
+			});
+		});
 	});
 
 	app.get("/rides", (req, res) => {
@@ -112,14 +84,14 @@ module.exports = (db) => {
 			if (err) {
 				return res.send({
 					error_code: "SERVER_ERROR",
-					message: "Unknown error",
+					message: "Unknown error"
 				});
 			}
 
 			if (rows.length === 0) {
 				return res.send({
 					error_code: "RIDES_NOT_FOUND_ERROR",
-					message: "Could not find any rides",
+					message: "Could not find any rides"
 				});
 			}
 
@@ -128,26 +100,23 @@ module.exports = (db) => {
 	});
 
 	app.get("/rides/:id", (req, res) => {
-		db.all(
-			`SELECT * FROM Rides WHERE rideID='${req.params.id}'`,
-			function (err, rows) {
-				if (err) {
-					return res.send({
-						error_code: "SERVER_ERROR",
-						message: "Unknown error",
-					});
-				}
-
-				if (rows.length === 0) {
-					return res.send({
-						error_code: "RIDES_NOT_FOUND_ERROR",
-						message: "Could not find any rides",
-					});
-				}
-
-				res.send(rows);
+		db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
+			if (err) {
+				return res.send({
+					error_code: "SERVER_ERROR",
+					message: "Unknown error"
+				});
 			}
-		);
+
+			if (rows.length === 0) {
+				return res.send({
+					error_code: "RIDES_NOT_FOUND_ERROR",
+					message: "Could not find any rides"
+				});
+			}
+
+			res.send(rows);
+		});
 	});
 
 	return app;
