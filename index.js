14dfc4f0e21f36a9847be66339bdd57f79576713
kaidi <kaidi@pand.ai>
2021-04-26 01:32:39 +0800

    Add CORS

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const port = 8010;
 
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
-var helmet = require("helmet");
 const buildSchemas = require("./src/schemas");
+const helmet = require("helmet");
+const cors = require('cors')
+
+var corsOptions = {
+    origin: "http://localhost:8010"
+  }
 
 db.serialize(() => {
 	buildSchemas(db);
@@ -12,6 +17,7 @@ db.serialize(() => {
 	const app = require("./src/app")(db);
 
 	app.use(helmet());
+    app.use(cors(corsOptions))
     
 	app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
